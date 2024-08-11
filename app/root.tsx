d39bc853082a93d2a9025b6924df02acafc8619e
deepso <deeps.o7o8.y@gmail.com>
2024-08-11 14:31:33 +0530

    add theme

--- app/root.tsx
+++ app/root.tsx
@@ -4,16 +4,37 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import "./tailwind.css";
+import clsx from "clsx";
+import {
+  PreventFlashOnWrongTheme,
+  ThemeProvider,
+  useTheme,
+} from "remix-themes";
+import { themeSessionResolver } from "./sessions.server";
+import { LoaderFunctionArgs } from "@remix-run/node";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+// Return the theme from the session storage using the loader
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request);
+  return {
+    theme: getTheme(),
+  };
+}
+
+function BaseLayout({ children }: { children: React.ReactNode }) {
+  const data = useLoaderData<typeof loader>();
+  const [theme] = useTheme();
+
   return (
-    <html lang="en">
+    <html lang="en" className={clsx(theme)}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body>
@@ -24,6 +45,16 @@ export function Layout({ children }: { children: React.ReactNode }) {
     </html>
   );
 }
+
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <BaseLayout>{children}</BaseLayout>
+    </ThemeProvider>
+  );
+};
 
 export default function App() {
   return <Outlet />;
